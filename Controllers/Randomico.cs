414b992cc28d97d0da3db83990f655e5c11f1797
Juan daniel Gonzales Aramayo <a2020116081@estudiantes.upsa.edu.bo>
2021-10-19 21:47:30 -0400

    Migre los modelos hacia la base de datos, ejecute y probe el swagger y añadi el randomico.

---
+++ Controllers/Randomico.cs
@@ -0,0 +1,44 @@
+﻿using Baraja2_juangonzales.Data;
+using Baraja2_juangonzales.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Baraja2_juangonzales.Controllers
+{
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RandomController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Naipe>> GetNaipe()
+        {
+
+            var list = await _context.Naipe.ToListAsync();
+
+            var max = list.Count;
+            int index = new Random().Next(0, max);
+
+            var Naipe = list[index];
+
+            if (Naipe == null)
+            {
+                return NoContent();
+            }
+
+            return Naipe;
+        }
+    }
+}
---
+++ Migrations/20211020014033_AddModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Baraja2_juangonzales.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Naipe",
+                columns: table => new
+                {
+                    NaipeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Naipe", x => x.NaipeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Naipe");
+        }
+    }
+}
